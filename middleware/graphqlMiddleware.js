0a0d7999f3b95b8fd04141a8782eef1bebc4df21
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-24 14:49:54 +0530

    graphql context configured for authorization headers

---
+++ middleware/graphqlMiddleware.js
@@ -0,0 +1,26 @@
+const JWT_SECRET = require('../config/appconfig').auth.jwt_secret
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('../server/schema/schema')
+const jwt = require('jsonwebtoken');
+
+const graphqlMiddleware = graphqlHTTP(async (request, response, graphQLParams) => {
+    const context = {
+      user: null
+    };
+    if (request.headers) {
+    const token = request.headers && request.headers.authorization.split(' ')[1];
+      try {
+        const decodedToken = await jwt.verify(token, JWT_SECRET);
+        context.user = decodedToken.userId;
+      } catch (error) {
+        console.error('JWT verification error: ', error.message);
+      }
+    }
+    return {
+      schema,
+      graphiql: true,
+      context,
+    };
+  });
+
+module.exports={"graphqlMiddleware": graphqlMiddleware  }
