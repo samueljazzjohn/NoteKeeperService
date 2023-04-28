8740866e38e86ec4feaccabe2d95baa97b48e324
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-28 13:16:37 +0530

    usermodel modified

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
     username:{type:String,required:true},
     email : {type:String,required:true,unique:true,index:true},
-    password : {type:String,required:true},
+    password : {type:String},
     notes: [{ type: mongoose.Types.ObjectId, ref: 'NoteModel' }]
 },{collection:'Users'}
 );
