913a15aae64f06f98ad3fce9f51f1e9ed1ebe800
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-28 17:55:28 +0530

    auth type changed

--- server/queries/getAuth.js
+++ server/queries/getAuth.js
@@ -1,10 +1,11 @@
 const {GraphQLObjectType,GraphQLID,GraphQLString, GraphQLSchema,GraphQLNonNull, GraphQLList} = require('graphql')
-
+const getUser=require('./getUser')
 
 const AuthType = new GraphQLObjectType({
     name: 'Auth',
     fields: {
       token: { type: GraphQLString },
+      user: { type: getUser },
     },
 });
 
