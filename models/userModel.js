0a990da0d208d4f164517e11628c27dab74c7036
samueljazzjohn <samueljazzjohn@gmail.com>
2023-04-29 17:55:18 +0530

    added new fields for storing reset password token

--- models/userModel.js
+++ models/userModel.js
@@ -4,6 +4,8 @@ const UserSchema = new mongoose.Schema({
     username:{type:String,required:true},
     email : {type:String,required:true,unique:true,index:true},
     password : {type:String},
+    resetPasswordToken:{type:String},
+    resetPasswordExpires:{type:Date},
     notes: [{ type: mongoose.Types.ObjectId, ref: 'NoteModel' }]
 },{collection:'Users'}
 );
